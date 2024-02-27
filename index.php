a03f447d17a57d53ac7b1ea82110f2220b63b053
gabwrel <82355336+gabwrel@users.noreply.github.com>
2024-02-27 17:35:22 +0800

    initial commit

--- index.php
+++ index.php
@@ -10,57 +10,65 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
+
 <script src="https://cdn.tailwindcss.com"></script>
 
-<div class="custom-bg flex items-center justify-center h-full">
-    <div class="container">
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="form" method="post">
-            <div class="container">
-                <h1 class="text-lg mobile:text-xl lg:text-2xl font-bold pt-10">Solutions you're<br>looking for</h1>
-            </div>
+<div class="custom-bg flex flex-col h-screen justify-between">
+    <div class="text-center pt-8">
+        <h1 class="text-3xl font-bold text-center">Solutions you're<br>looking for</h1>
+    </div>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="form" method="post">
 
-            <div class="container">
-                <?php
-                $categoryQuery = "SELECT * FROM category";
-                $categoryResult = mysqli_query($conn, $categoryQuery);
+    <div class="flex flex-col items-center space-y-4">
+        <?php
+            $categoryQuery = "SELECT * FROM category";
+            $categoryResult = mysqli_query($conn, $categoryQuery);
 
-                while ($categoryRow = mysqli_fetch_assoc($categoryResult)) {
-                    $categoryID     = $categoryRow['categoryID'];
-                    $categoryName   = $categoryRow['categoryName'];
-                    ?>
-                    <div class="mb-4">
-                        <button type="submit" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-8 py-2.5 mb-2" name="category" value="<?php echo $categoryID; ?>">
-                            <?php echo $categoryName; ?>
-                        </button>
-                    </div>
-                    <?php
-                }
+            while ($categoryRow = mysqli_fetch_assoc($categoryResult)) {
+                $categoryID     = $categoryRow['categoryID'];
+                $categoryName   = $categoryRow['categoryName'];
                 ?>
-            </div>
-        </form>
+                <div class="buttons">
+                    <button type="submit" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" name="category" value="<?php echo $categoryID; ?>">
+                        <?php echo $categoryName; ?>
+                    </button>
+                </div>
+                <?php
+            }
+        ?>
+    </div>
+    </form>
+    <div class="bottom-space">
     </div>
 </div>
 
 <style>
+    .bottom-space{
+        height: 300px;
+    }
 
-    .mb-4 { 
-        max-width: 350px;
-        min-width: 200PX;
+    .buttons { 
+        max-width: 360px;
+        min-width: 200px;
         width: 100%;
     }
 
     button{
         display: block;
         width: 100%;
+        z-index: 10;
     }
 
+
     .custom-bg {
-        background-image: url('./assets/images/bg-1.jpg');
+        background-image: url('./assets/images/bg.jpg');
         background-repeat: no-repeat;
         background-size: cover;
         background-position: center center;
         width: 100%;
-        height: 100vh; /* Set the height of the body to fill the viewport */
-        margin: 0; /* Remove default margin */
+        height: 100vh;
+        margin: 0;
     }
+
 </style>
+
