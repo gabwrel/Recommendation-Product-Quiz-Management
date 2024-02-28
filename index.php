bab096e39c55b1ccc0f2199f3f5dd11ecb2e728f
gabwrel <82355336+gabwrel@users.noreply.github.com>
2024-02-28 10:50:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,74 +1,80 @@
-<?php
-session_start();
-$pageTitle = "Quiz";
-include 'db.php';
+    <?php
+    session_start();
+    $pageTitle = "Quiz";
+    include 'db.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $_SESSION['selectedCategory'] = $_POST['category'];
-    header("Location: category-page.php");
-    exit();
-}
-?>
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $_SESSION['selectedCategory'] = $_POST['category'];
+        header("Location: category-page.php");
+        exit();
+    }
+    ?>
+
+    <link rel="stylesheet" href="./assets/output.css">
 
 
-<script src="https://cdn.tailwindcss.com"></script>
+    <div class="custom-bg flex flex-col h-screen justify-between">
+        <div class="text-center">
+            <h1 class="title text-4xl font-bold text-center">Solutions you're<br>looking for</h1>
+        </div>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="form" method="post">
 
-<div class="custom-bg flex flex-col h-screen justify-between">
-    <div class="text-center pt-8">
-        <h1 class="text-3xl font-bold text-center">Solutions you're<br>looking for</h1>
+        <div class="flex flex-col items-center space-y-4">
+            <?php
+                $categoryQuery = "SELECT * FROM category";
+                $categoryResult = mysqli_query($conn, $categoryQuery);
+
+                while ($categoryRow = mysqli_fetch_assoc($categoryResult)) {
+                    $categoryID     = $categoryRow['categoryID'];
+                    $categoryName   = $categoryRow['categoryName'];
+                    ?>
+                    <div class="buttons">
+                        <button type="submit" class="text-white text-2xl bg-gradient-to-r from-bc1 via-bc2 to-bc3 border-amber-700 border-2 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 rounded-3xl text-sm px-5 py-2.5 text-center me-2 mb-2" name="category" value="<?php echo $categoryID; ?>">
+                            <?php echo $categoryName; ?>
+                        </button>
+                    </div>
+                    <?php
+                }
+            ?>
+        </div>
+        </form>
+        <div class="bottom-space">
+        </div>
     </div>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="form" method="post">
 
-    <div class="flex flex-col items-center space-y-4">
-        <?php
-            $categoryQuery = "SELECT * FROM category";
-            $categoryResult = mysqli_query($conn, $categoryQuery);
+    <style>
+        .title{
+        }
 
-            while ($categoryRow = mysqli_fetch_assoc($categoryResult)) {
-                $categoryID     = $categoryRow['categoryID'];
-                $categoryName   = $categoryRow['categoryName'];
-                ?>
-                <div class="buttons">
-                    <button type="submit" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" name="category" value="<?php echo $categoryID; ?>">
-                        <?php echo $categoryName; ?>
-                    </button>
-                </div>
-                <?php
-            }
-        ?>
-    </div>
-    </form>
-    <div class="bottom-space">
-    </div>
-</div>
+        .bottom-space{
+            height: 300px;
+        }
 
-<style>
-    .bottom-space{
-        height: 300px;
-    }
+        .buttons { 
+            max-width: 360px;
+            min-width: 200px;
+            width: 100%;
+        }
 
-    .buttons { 
-        max-width: 360px;
-        min-width: 200px;
-        width: 100%;
-    }
-
-    button{
-        display: block;
-        width: 100%;
-        z-index: 10;
-    }
+        button{
+            display: block;
+            width: 100%;
+            z-index: 10;
+        }
 
 
-    .custom-bg {
-        background-image: url('./assets/images/bg.jpg');
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-position: center center;
-        width: 100%;
-        height: 100vh;
-        margin: 0;
-    }
+        .custom-bg {
+            background-image: url('./assets/images/bg.jpg');
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center center;
+            width: 100%;
+            height: 100vh;
+            margin: 0;
+            min-width: 360px !important;
+            min-height: 800px !important; 
+            padding-top: clamp(20px, 40%, 40px);
+        }
 
-</style>
+    </style>
 
